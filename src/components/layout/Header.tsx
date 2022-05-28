4425758e0d08b93e2f3d0721330f0e05a67e250d
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-05-28 12:34:20 +0900

    🚧 add button that toggles color mode (light and dark)

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,15 +1,29 @@
+import { IconButton, useColorMode } from '@chakra-ui/react';
 import { useTranslation } from 'next-i18next';
 import type { FC } from 'react';
+import { useEffect } from 'react';
+import { FaMoon, FaSun } from 'react-icons/fa';
 
 import { StyledLink } from '@/components/ui/StyledLink';
 
 export const Header: FC = () => {
   const [t] = useTranslation('common');
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  useEffect(() => {
+    document.documentElement.classList.add(colorMode);
+    document.documentElement.classList.remove(colorMode === 'light' ? 'dark' : 'light');
+  }, [colorMode]);
 
   return (
     <>
       <header className="h-8">
         <StyledLink href="/">{t('header.backToTop')}</StyledLink>
+        <IconButton
+          icon={colorMode === 'light' ? <FaSun /> : <FaMoon />}
+          onClick={() => toggleColorMode()}
+          aria-label={t('header.toggleColorMode')}
+        />
       </header>
     </>
   );
