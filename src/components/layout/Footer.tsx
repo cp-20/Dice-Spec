7c3e6e881d4ea4d169ab6ef88f8dc673da05b050
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-05-28 13:00:42 +0900

    🚧 move twitter link from footer to header

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,8 +1,5 @@
 import { useTranslation } from 'next-i18next';
 import type { FC } from 'react';
-import { FaTwitter } from 'react-icons/fa';
-
-import { StyledLink } from '@/components/ui/StyledLink';
 
 export const Footer: FC = () => {
   const [t] = useTranslation('common');
@@ -11,9 +8,6 @@ export const Footer: FC = () => {
     <>
       <footer className="bg-gray-500/20 py-2 px-4">
         <div>
-          <StyledLink href="https://twitter.com/__cp20__" icon="none">
-            <FaTwitter title={t('footer.twitter')} />
-          </StyledLink>
           <p>{t('copyright')}</p>
         </div>
       </footer>
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,6 +3,7 @@ import { useTranslation } from 'next-i18next';
 import type { FC } from 'react';
 import { useEffect } from 'react';
 import { FaMoon, FaSun } from 'react-icons/fa';
+import { FaTwitter } from 'react-icons/fa';
 
 import { StyledLink } from '@/components/ui/StyledLink';
 
@@ -21,7 +22,12 @@ export const Header: FC = () => {
         <header className="flex py-2 px-4">
           <StyledLink href="/">{t('header.backToTop')}</StyledLink>
 
-          <div className="ml-auto">
+          <div className="ml-auto flex items-center space-x-4">
+            {/* Developer's Twitter */}
+            <StyledLink href="https://twitter.com/__cp20__" icon="none">
+              <FaTwitter title={t('footer.twitter')} />
+            </StyledLink>
+
             {/* toggleColorMode Button */}
             <IconButton
               size="sm"
