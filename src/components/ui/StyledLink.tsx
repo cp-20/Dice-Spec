7992757a1516a8c8815bf42458b5a4ab0768913d
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-05-28 11:50:36 +0900

    🚧 add "icon" property to StyledLink

--- src/components/ui/StyledLink.tsx
+++ src/components/ui/StyledLink.tsx
@@ -1,14 +1,20 @@
 /* eslint-disable react/jsx-no-target-blank */
 import Link from 'next/link';
 import type { FC, ReactNode } from 'react';
+import { FiExternalLink } from 'react-icons/fi';
+
+import { cx } from '@/features/utils/cx';
+
+export type iconStyle = 'none';
 
 export type StyledLinkProps = {
   href: string;
   children: ReactNode;
   className?: string;
+  icon?: iconStyle;
 };
 
-export const StyledLink: FC<StyledLinkProps> = ({ href, className, children }) => {
+export const StyledLink: FC<StyledLinkProps> = ({ href, className, children, icon }) => {
   const isExternal = href.startsWith('http');
 
   return (
@@ -18,9 +24,10 @@ export const StyledLink: FC<StyledLinkProps> = ({ href, className, children }) =
           href={href}
           rel={isExternal ? 'noreferrer noopener' : ''}
           target={isExternal ? '_blank' : ''}
-          className={className}
+          className={cx(className, 'inline-flex items-center space-x-2')}
         >
-          {children}
+          <span>{children}</span>
+          {icon !== 'none' && isExternal && <FiExternalLink />}
         </a>
       </Link>
     </>
