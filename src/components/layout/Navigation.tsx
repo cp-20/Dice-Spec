5cea2c15f6555a8063ab59cea7b7f8d5abb335b1
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-05-29 19:04:04 +0900

    🎨 highlight current tab

--- src/components/layout/Navigation.tsx
+++ src/components/layout/Navigation.tsx
@@ -1,4 +1,5 @@
 import { cx } from '@chakra-ui/utils';
+import { useRouter } from 'next/router';
 import { useTranslation } from 'next-i18next';
 import type { FC } from 'react';
 import { BsDice5 } from 'react-icons/bs';
@@ -7,11 +8,23 @@ import { FaSearch } from 'react-icons/fa';
 import type { StyledLinkProps } from '@/components/ui/StyledLink';
 import { StyledLink } from '@/components/ui/StyledLink';
 
-const NavigationLink: FC<StyledLinkProps> = ({ children, className, ...props }) => (
-  <StyledLink className={cx(className, 'py-2 px-4 duration-100 hover:bg-gray-400/10')} {...props}>
-    {children}
-  </StyledLink>
-);
+const NavigationLink: FC<StyledLinkProps> = ({ children, href, className, ...props }) => {
+  const { pathname } = useRouter();
+
+  return (
+    <StyledLink
+      className={cx(
+        className,
+        'py-2 px-4 duration-100',
+        pathname === href ? 'bg-blue-100 dark:bg-blue-300/30' : 'hover:bg-gray-400/10'
+      )}
+      href={href}
+      {...props}
+    >
+      {children}
+    </StyledLink>
+  );
+};
 
 export const Navigation: FC = () => {
   const [t] = useTranslation('common');
