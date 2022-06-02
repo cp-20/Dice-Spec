44b36b09901def2bb550c649059406e37803126f
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-06-02 18:11:56 +0900

    🎨 a little styling for darkmode

--- src/components/model/DiceResult.tsx
+++ src/components/model/DiceResult.tsx
@@ -32,9 +32,9 @@ export const DiceResult: FC<{ result: diceResult[] }> = ({ result }) => {
     <>
       <div
         ref={containerRef}
-        className="my-4 flex max-h-[10rem] flex-col space-y-1 overflow-y-auto rounded-md border-2 border-gray-100 px-4 py-2 text-sm"
+        className="my-4 flex max-h-[10rem] flex-col space-y-1 overflow-y-auto rounded-md border-2 border-gray-500/10 px-4 py-2 text-sm"
       >
-        <p className="text-md font-bold text-gray-600">{t('output')}</p>
+        <p className="text-md font-bold text-gray-600 dark:text-gray-300">{t('output')}</p>
         {result.map((item, index) => (
           <div key={index} className="flex space-x-2">
             <p className="text-gray-500">{item.system}</p>
