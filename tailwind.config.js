ae031d8198de3279751ce3458039a87b0485ba42
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-06-04 12:52:24 +0900

    🔧 fix tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,6 @@ module.exports = {
   content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
-    colors: {
-      ...defaultTheme.colors,
-      transparent: 'transparent',
-      current: 'currentColor',
-    },
     fontFamily: {
       ...defaultTheme.fontFamily,
       ZenKaku: ['Zen Kaku Gothic Antique', 'sans-serif'],
