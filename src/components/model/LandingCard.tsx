c425e025957f6922d4af6430ba8eb7806839e30d
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-06-04 13:53:51 +0900

    🚧 implement landing page

---
+++ src/components/model/LandingCard.tsx
@@ -0,0 +1,24 @@
+import { LinkBox, LinkOverlay } from '@chakra-ui/react';
+import Link from 'next/link';
+import type { FC, ReactNode } from 'react';
+
+export const LandingCard: FC<{ title: string; content: string; href: string; icon: ReactNode }> = ({
+  title,
+  content,
+  href,
+  icon,
+}) => (
+  <>
+    <LinkBox className="flex-1 rounded-md border p-4">
+      <div className="flex items-center space-x-4 md:flex-col md:space-x-0 md:space-y-4">
+        <div className="text-3xl sm:text-4xl md:text-5xl">{icon}</div>
+        <div className="flex flex-col space-y-2 md:items-center">
+          <Link href={href} passHref>
+            <LinkOverlay className="text-lg font-bold">{title}</LinkOverlay>
+          </Link>
+          <p className="text-gray-600 dark:text-gray-400">{content}</p>
+        </div>
+      </div>
+    </LinkBox>
+  </>
+);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,45 @@
 import type { NextPage } from 'next';
+import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import { BsDice5 } from 'react-icons/bs';
+import { FaFileExport, FaSearch } from 'react-icons/fa';
+
+import { Footer } from '@/components/layout/Footer';
+import { Header } from '@/components/layout/Header';
+import { LandingCard } from '@/components/model/LandingCard';
 
 const Home: NextPage = () => {
+  const [t] = useTranslation(['index', 'common']);
+
   return (
     <>
-      <div>landing page</div>
+      <Header isDesktop={true} />
+      <div className="min-h-[calc(100vh-5.5rem)] px-4">
+        <h1 className="py-8 text-center font-ZenKaku text-4xl sm:text-5xl md:text-6xl">{t('common:title')}</h1>
+        <p className="text-center">{t('description')}</p>
+
+        <div className="my-8 flex flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-4">
+          <LandingCard
+            title={t('features.1.title')}
+            content={t('features.1.description')}
+            href="/expect"
+            icon={<FaSearch />}
+          />
+          <LandingCard
+            title={t('features.2.title')}
+            content={t('features.2.description')}
+            href="/dice"
+            icon={<BsDice5 />}
+          />
+          <LandingCard
+            title={t('features.3.title')}
+            content={t('features.3.description')}
+            href="/ccfolia"
+            icon={<FaFileExport />}
+          />
+        </div>
+      </div>
+      <Footer />
     </>
   );
 };
