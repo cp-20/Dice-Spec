c3d89b7353aba1d463d482f7a674374875f8464b
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-06-04 17:21:41 +0900

    🎨 styling LandingCard for darkmode

--- src/components/model/LandingCard.tsx
+++ src/components/model/LandingCard.tsx
@@ -11,7 +11,7 @@ export const LandingCardLink: FC<{ title: string; content: string; href: string;
   icon,
 }) => (
   <>
-    <LinkBox className="flex-1 rounded-md border p-4 shadow-blue-50 transition-all hover:bg-blue-50 hover:shadow-md">
+    <LinkBox className="flex-1 rounded-md border p-4 shadow-blue-50 transition-all hover:bg-blue-50 hover:shadow-md dark:shadow-none dark:hover:bg-blue-50/5">
       <div className="flex items-center space-x-4 md:flex-col md:space-x-0 md:space-y-4">
         <div className="text-3xl sm:text-4xl md:text-5xl">{icon}</div>
         <div className="flex flex-col space-y-2 md:items-center">
@@ -27,7 +27,7 @@ export const LandingCardLink: FC<{ title: string; content: string; href: string;
 
 export const LandingCard: FC<{ title: string; content: string }> = ({ title, content }) => (
   <>
-    <div className="flex-1 rounded-md border p-4 shadow-blue-50 transition-all hover:shadow-md">
+    <div className="flex-1 rounded-md border p-4 shadow-blue-50 transition-all hover:shadow-md dark:hover:bg-blue-700/5">
       <p className="mb-2 text-lg font-bold">{title}</p>
       <p className="text-sm text-gray-600 dark:text-gray-400">{content}</p>
     </div>
@@ -39,7 +39,7 @@ export const LandingLink: FC<{ icon: ReactNode; label: string; href: string }> =
     <StyledLink
       href={href}
       icon={icon}
-      className="rounded-md px-4 py-1 shadow-blue-50  transition-all hover:bg-blue-50 hover:shadow-sm"
+      className="rounded-md px-4 py-1 shadow-blue-50  transition-all hover:bg-blue-50 hover:shadow-sm dark:hover:bg-blue-50/5"
     >
       {label}
     </StyledLink>
