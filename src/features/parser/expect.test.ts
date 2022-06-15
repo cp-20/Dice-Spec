cbe72c479e1a17ee13c581c82bca3ee2d4969b06
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-06-15 17:07:53 +0900

    ✅ write test code for dice expecter

---
+++ src/features/parser/expect.test.ts
@@ -0,0 +1,31 @@
+import { calcExpectedValue } from '@/features/parser/expect';
+
+describe('calcExpectedValue', () => {
+  it('should return expected value', () => {
+    expect(calcExpectedValue('1d6')).toEqual(
+      expect.objectContaining({
+        range: {
+          min: 1,
+          max: 6,
+        },
+        CI: {
+          min: 1,
+          max: 6,
+        },
+      })
+    );
+
+    expect(calcExpectedValue('1d6-1')).toEqual(
+      expect.objectContaining({
+        range: {
+          min: 0,
+          max: 7,
+        },
+        CI: {
+          min: 0,
+          max: 7,
+        },
+      })
+    );
+  });
+});
