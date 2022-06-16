71d18d6efceffa608c784ef0e982d7ec41f55817
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2022-06-16 19:48:00 +0900

    ✅ add tests

--- src/features/parser/expect.test.ts
+++ src/features/parser/expect.test.ts
@@ -3,10 +3,10 @@ import { expect } from '@jest/globals';
 import { calcExpectedValue } from '@/features/parser/expect';
 
 describe('Dice Expecter', () => {
-  it('calcExpectedValue', () => {
+  it('calcExpectedValue', async () => {
     const DiceV = 35 / 12;
 
-    expect(calcExpectedValue('1d6')).toEqual({
+    expect(await calcExpectedValue('1d6')).toEqual({
       mean: 3.5,
       variance: expect.closeTo(DiceV, 1),
       SD: expect.closeTo(Math.sqrt(DiceV), 1),
@@ -19,7 +19,7 @@ describe('Dice Expecter', () => {
         max: expect.closeTo(5.9, 1),
       },
     });
-    expect(calcExpectedValue('1d6 + 1d6 - 3')).toEqual({
+    expect(await calcExpectedValue('1d6 + 1d6 - 3')).toEqual({
       mean: 4,
       variance: expect.closeTo(DiceV * 2, 1),
       SD: expect.closeTo(Math.sqrt(DiceV * 2), 1),
@@ -32,7 +32,7 @@ describe('Dice Expecter', () => {
         max: expect.closeTo(8.9, 1),
       },
     });
-    expect(calcExpectedValue('1d6 * 1d6 * 2 + 5')).toEqual({
+    expect(await calcExpectedValue('1d6 * 1d6 * 2 + 5')).toEqual({
       mean: 3.5 ** 2 * 2 + 5,
       variance: expect.closeTo((((3.5 ** 2 * 2 + DiceV) * 35) / 12) * 4, 1),
       SD: expect.closeTo(Math.sqrt((((3.5 ** 2 * 2 + DiceV) * 35) / 12) * 4), 1),
@@ -45,7 +45,7 @@ describe('Dice Expecter', () => {
         max: expect.closeTo(76.9, 1),
       },
     });
-    expect(calcExpectedValue('3d6 + 1d100 + 1')).toEqual({
+    expect(await calcExpectedValue('3d6 + 1d100 + 1')).toEqual({
       mean: 3.5 * 3 + 50.5 + 1,
       variance: expect.closeTo(DiceV * 3 + (100 ** 2 - 1) / 12, 1),
       SD: expect.closeTo(Math.sqrt(DiceV * 3 + (100 ** 2 - 1) / 12), 1),
@@ -56,6 +56,19 @@ describe('Dice Expecter', () => {
       CI: {
         min: expect.closeTo(14, 1),
         max: expect.closeTo(110, 1),
+      },
+    });
+    expect(await calcExpectedValue('6-1d6')).toEqual({
+      mean: 6 - 3.5,
+      variance: expect.closeTo(DiceV, 1),
+      SD: expect.closeTo(Math.sqrt(DiceV), 1),
+      range: {
+        min: 0,
+        max: 5,
+      },
+      CI: {
+        min: expect.closeTo(0.1, 1),
+        max: expect.closeTo(4.9, 1),
       },
     });
   });
