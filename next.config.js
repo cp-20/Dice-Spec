50174fcf38cf00135328e47019b9a7b1d5401e20
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-03-28 07:01:38 +0900

    🚧 開発環境ではPWAを無効化

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  // disable: process.env.NODE_ENV === 'development',
+  disable: process.env.NODE_ENV === 'development',
 });
 
 const localeSubpaths = {};
